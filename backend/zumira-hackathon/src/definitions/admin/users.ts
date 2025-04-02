e10342761453f78e24ea4a7596eecf57cb495abc
Gabriel Brevilieri <gbrevilieri@gmail.com>
2025-04-02 17:06:39 -0300

    fix: error creating user

--- backend/zumira-hackathon/src/definitions/admin/users.ts
+++ backend/zumira-hackathon/src/definitions/admin/users.ts
@@ -1,23 +1,23 @@
-import { z } from "zod"
+import { z } from "zod";
 
 export const CreateUserSchema = z.object({
   email: z.string().email(),
   name: z.string().min(1),
   roleId: z.string().uuid(),
-  companyId: z.string().uuid().optional()
-})
+  companyId: z.string().cuid().optional(),
+});
 
 export const FindByEmailSchema = z.object({
-  email: z.string().email()
-})
+  email: z.string().email(),
+});
 
 export const UpdateUserSchema = z.object({
   name: z.string().optional(),
   roleId: z.string().uuid().optional(),
-  companyId: z.string().cuid().optional()
-})
+  companyId: z.string().cuid().optional(),
+});
 
 export const CreateCompanySchema = z.object({
   name: z.string().min(1),
-  email: z.string().email()
-})
+  email: z.string().email(),
+});
