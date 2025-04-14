f2618f708c24b83b7b16617e5e286152d7c725f7
Gabriel Brevilieri <gbrevilieri@gmail.com>
2025-04-14 17:34:24 -0300

    feat: added horizontal rule button

--- frontend/zumira-hackathon/src/components/ui/rich-text-area.tsx
+++ frontend/zumira-hackathon/src/components/ui/rich-text-area.tsx
@@ -5,7 +5,8 @@ import Highlight from "@tiptap/extension-highlight";
 import Typography from "@tiptap/extension-typography";
 import { Editor, EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import { Bold, Italic, List, ListOrdered, Type, UnderlineIcon } from "lucide-react";
+import HorizontalRule from "@tiptap/extension-horizontal-rule";
+import { Bold, Italic, List, ListOrdered, Minus, Type, UnderlineIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 import Underline from "@tiptap/extension-underline";
 import { Divider } from "../custom/divider";
@@ -21,10 +22,28 @@ interface RichTextAreaProps {
 
 interface OverflowButton {
   editor: Editor | null;
+  icon: React.ElementType;
   children?: ReactNode;
 }
 
-function CollapsedContent({ editor, children }: OverflowButton) {
+interface EditorButtonProps {
+  highlight?: boolean;
+  icon: React.ElementType;
+  onClick?: () => void;
+}
+
+function EditorButton({ highlight, icon: Icon, onClick }: EditorButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(highlight ? "text-gray-700" : "text-gray-400", "cursor-pointer hover:text-gray-500")}
+    >
+      <Icon className="size-4.5" />
+    </button>
+  );
+}
+
+function CollapsedContent({ editor, icon: Icon, children }: OverflowButton) {
   const [open, setOpen] = useState<boolean>(false);
   const divRef = useRef<HTMLDivElement>(null);
   const buttonRef = useRef<HTMLButtonElement>(null);
@@ -56,7 +75,7 @@ function CollapsedContent({ editor, children }: OverflowButton) {
         className={cn(open || isTitle ? "text-gray-700" : "text-gray-400", "cursor-pointer")}
         onClick={() => setOpen((prev) => !prev)}
       >
-        <Type className="size-4.5" />
+        <Icon className="size-4.5" />
       </button>
       <div
         className="relative h-full overflow-clip duration-200 bg-black/5 rounded-md"
@@ -81,7 +100,7 @@ export function RichTextArea({ id, value, onChange }: RichTextAreaProps) {
   }, 500);
 
   const editor = useEditor({
-    extensions: [StarterKit, Highlight, Typography, Underline],
+    extensions: [StarterKit, Highlight, Typography, Underline, HorizontalRule],
     content: marked(value ?? ""),
     onUpdate({ editor }) {
       const html = editor.getHTML();
@@ -96,67 +115,52 @@ export function RichTextArea({ id, value, onChange }: RichTextAreaProps) {
       className="relative overflow-clip w-full flex flex-col rounded-xl border border-gray-300 bg-transparent text-base shadow-xs transition-[color,box-shadow] outline-none selection:bg-primary selection:text-primary-foreground focus-within:border-primary-300 focus-within:shadow-focus-ring aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive"
     >
       <div className="flex flex-row items-center p-2 gap-2 bg-gray-100 h-10 border-b border-gray-300">
-        <button
+        <EditorButton
           onClick={() => editor?.chain().focus().toggleBold().run()}
-          className={cn(editor?.isActive("bold") ? "text-gray-700" : "text-gray-400", "cursor-pointer")}
-        >
-          <Bold className="size-4.5" />
-        </button>
-        <button
+          highlight={editor?.isActive("bold")}
+          icon={Bold}
+        />
+        <EditorButton
           onClick={() => editor?.chain().focus().toggleItalic().run()}
-          className={cn(editor?.isActive("italic") ? "text-gray-700" : "text-gray-400", "cursor-pointer")}
-        >
-          <Italic className="size-4.5" />
-        </button>
-        <button
+          highlight={editor?.isActive("italic")}
+          icon={Italic}
+        />
+        <EditorButton
           onClick={() => editor?.chain().focus().toggleUnderline().run()}
-          className={cn(editor?.isActive("underline") ? "text-gray-700" : "text-gray-400", "cursor-pointer")}
-        >
-          <UnderlineIcon className="size-4.5" />
-        </button>
+          highlight={editor?.isActive("underline")}
+          icon={UnderlineIcon}
+        />
         <Divider />
-        <button
+        <EditorButton
           onClick={() => editor?.chain().focus().toggleBulletList().run()}
-          className={cn(editor?.isActive("bulletList") ? "text-gray-700" : "text-gray-400", "cursor-pointer")}
-        >
-          <List className="size-4.5" />
-        </button>
-        <button
+          highlight={editor?.isActive("bulletList")}
+          icon={List}
+        />
+        <EditorButton
           onClick={() => editor?.chain().focus().toggleOrderedList().run()}
-          className={cn(editor?.isActive("orderedList") ? "text-gray-700" : "text-gray-400", "cursor-pointer")}
-        >
-          <ListOrdered className="size-4.5" />
-        </button>
+          highlight={editor?.isActive("orderedList")}
+          icon={ListOrdered}
+        />
         <Divider />
-        <CollapsedContent editor={editor}>
-          <button
+        <CollapsedContent editor={editor} icon={Type}>
+          <EditorButton
             onClick={() => editor?.chain().focus().toggleHeading({ level: 1 }).run()}
-            className={cn(
-              editor?.isActive("heading", { level: 1 }) ? "text-gray-700" : "text-gray-400",
-              "cursor-pointer"
-            )}
-          >
-            <span className="font-semibold text-center size-4.5">H1</span>
-          </button>
-          <button
+            highlight={editor?.isActive("heading", { level: 1 })}
+            icon={() => <span className="font-semibold text-center size-4.5">H1</span>}
+          />
+          <EditorButton
             onClick={() => editor?.chain().focus().toggleHeading({ level: 2 }).run()}
-            className={cn(
-              editor?.isActive("heading", { level: 2 }) ? "text-gray-700" : "text-gray-400",
-              "cursor-pointer"
-            )}
-          >
-            <span className="font-semibold text-center size-4.5">H2</span>
-          </button>
-          <button
+            highlight={editor?.isActive("heading", { level: 2 })}
+            icon={() => <span className="font-semibold text-center size-4.5">H2</span>}
+          />
+          <EditorButton
             onClick={() => editor?.chain().focus().toggleHeading({ level: 3 }).run()}
-            className={cn(
-              editor?.isActive("heading", { level: 3 }) ? "text-gray-700" : "text-gray-400",
-              "cursor-pointer"
-            )}
-          >
-            <span className="font-semibold text-center size-4.5">H3</span>
-          </button>
+            highlight={editor?.isActive("heading", { level: 3 })}
+            icon={() => <span className="font-semibold text-center size-4.5">H3</span>}
+          />
         </CollapsedContent>
+        <Divider />
+        <EditorButton onClick={() => editor?.chain().focus().setHorizontalRule().run()} icon={Minus} />
       </div>
       <EditorContent editor={editor} className="prose markdown w-full focus:outline-none min-h-40" />
     </div>
